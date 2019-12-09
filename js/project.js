a5fc6fd940094dcf804075084c48c21add42272f
sopheaktra.ros <sopheaktra.ros@pnc.lan>
2019-12-09 07:26:00 +0700

    display data on interface

--- js/project.js
+++ js/project.js
@@ -1,5 +1,5 @@
-$(document).ready(function(){
-  
+$('#hide').hide();//hide input calulate
+$(document).ready(function(){ 
     var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
     $('#choose').on('change', function(){
         $.ajax({
@@ -8,22 +8,89 @@ $(document).ready(function(){
             success: function(data){
                 var getData = $('#choose').val();
                 var result = "";
+                var output = "";
                 data.recipes.forEach(element => {
                     if(getData == element.id){
                     result +=`
                     <div class="row">
-                        <div class="col-6">
+                        <div class="col-2"></div>
+                        <div class="col-4">
                             <h1>${element.name}</h1>
                         </div>
-                        <div class="col-6">
+                        <div class="col-3">
                             <img src="${element.iconUrl}" class="img-fluid" style="width: 200px; height: 150px;"/>
                         </div>
+                        <div class="col-3"></div>
                     </div>
                     `;
-                    }
-                });
+
+                    $('#hide').show();//show input calulate
+
+                    element.ingredients.forEach(item =>{
+                    $('#Ingredients').html('Ingredients');
+                    output +=`
+                      <tr>
+                        <td><img src="${item.iconUrl}" width="30"></td>
+                        <td>${item.quantity}</td>
+                        <td>${(item.unit[0]).toLowerCase()}</td>
+                        <td>${item.name}</td>
+                      </tr>
+                      `;
+                    });//foreach 2 loop for ingredients
+                    $('#output').html(output);
+
+                    $('#Instructions').html('Instructions');
+                    
+
+                    };//condition if esle
+                });//foreach
                 $('#result').html(result);
-            } 
-        }); 
-    });
+
+            } //success
+        });//ajax
+    });//function change
+
+});//function ready
+
+/////////////////////////////////////////////////////////
+//calutate number
+$('#up').on('click', function(){
+    var getInput = $('#getInput').val();
+    userUp(getInput); 
 });
+
+$('#donw').on('click', function(){
+    var getInput = $('#getInput').val();
+    userDonw(getInput); 
+});
+
+function userUp(up){
+    var getVarlueUp = parseInt(up) + 1;
+    if(getVarlueUp <= 15){
+        $('#getInput').val(getVarlueUp);
+        multiple(getVarlueUp);
+       
+    }
+}
+
+function userDonw(donw){
+    var getVarlueDonw = parseInt(donw) - 1;
+    if(getVarlueDonw >= 0){
+        $('#getInput').val(getVarlueDonw);
+        multiple(getVarlueDonw);
+        
+    }
+}
+
+function getValueFromDB(){
+
+}
+
+function multiple(calculate){
+    var muls = calculate * 2;
+    output(muls);
+}
+
+function output(out){
+    $('#result').html(out);
+}
