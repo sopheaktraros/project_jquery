966a0da1951235c5dd2ff797193a8ead6c78a361
sopheaktra.ros <sopheaktra.ros@pnc.lan>
2019-12-10 17:47:13 +0700

    rewrite code by use function

--- js/project.js
+++ js/project.js
@@ -1,65 +1,21 @@
-$('#hide').hide();//hide input calulate
+$('#inputCalulate').hide();//hide input calulate
 $('#verical').hide();//hide verical line
-$(document).ready(function(){
-    requerstApi();//requerst name of element from api
-    var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
-    $('#choose').on('change', function(){
-        $('#hide').show();//show input calulate
-        $('#verical').show();// show veircal line
-        $('#Instructions').html('Instructions');//Instructions
-        $('#Ingredients').html('Ingredients');//Ingredients 
-        $.ajax({
-            dataType: 'json',
-            url: url,
-            success: function(data){
-                var result = "";
-                var output = "";
-                data.recipes.forEach(element => {
-                    var getData = $('#choose').val();
-                    if(getData == element.id){
-                    result +=`
-                    <div class="row">
-                        <div class="col-2"></div>
-                        <div class="col-4">
-                            <h1>${element.name}</h1>
-                        </div>
-                        <div class="col-3">
-                            <img src="${element.iconUrl}" class="img-fluid" style="width: 200px; height: 150px;"/>
-                        </div>
-                        <div class="col-3"></div>
-                    </div>
-                    `;
-
-                    element.ingredients.forEach(item =>{
-                    output +=`
-                      <tr>
-                        <td><img src="${item.iconUrl}" width="30"></td>
-                        <td>${item.quantity}</td>
-                        <td>${(item.unit[0]).toLowerCase()}</td>
-                        <td>${item.name}</td>
-                      </tr>
-                      `;
-                    });//foreach 2 (loop for ingredients)
-                    $('#output').html(output);
-
-                    //Instructions 
-                    
-
-                    };//condition if esle
-                });//foreach
-                $('#result').html(result);
-
-            } //success
-        });//ajax
-    });//function on change
-});//function ready
-
-//get url
 function getUrl(){
     var url = "https://raw.githubusercontent.com/radytrainer/test-api/master/test.json";
     return url;
 }
-//requerst api
+
+var allData = [];
+$(document).ready(function () {
+   requerstApi();
+$('#select').on('change', function(){
+    $('#inputCalulate').show();//show input calulate
+    $('#verical').show();//show verical line
+    var getValueSelect = $('#select').val();
+    getRecipes(getValueSelect);
+   })
+});
+
 function requerstApi(){
     $.ajax({
         dataType: 'json',
@@ -68,48 +24,67 @@ function requerstApi(){
         error: () => console.log('error'),
     })
 }
-//loop
 function chooseRecipe(recipe){
+    allData = recipe;
     var option = "";
     recipe.forEach(element => {
         option +=`
             <option value="${element.id}">${element.name}</option>
         `;
     });
-    $('#choose').append(option);
+    $('#select').append(option);
+}
+function getRecipes(id) {
+    allData.forEach( item =>{
+        if(item.id == id){
+            eachRecipes(item.name, item.iconUrl);
+            eachIngredients(item.ingredients);
+            eachStep(item.instructions);
+        }
+    })
 }
 
-/////////////////////////////////////////////////////////
-//calutate number
-$('#up').on('click', function(){
-    var getInput = $('#getInput').val();
-    userUp(getInput); 
-});
+function eachRecipes(name, img){
+    var result = "";
+    result +=`
+    <div class="row">
+        <div class="col-2"></div>
+        <div class="col-4">
+            <h1>${name}</h1>
+        </div>
+        <div class="col-3">
+            <img src="${img}" class="img-fluid" style="width: 200px; height: 150px;"/>
+        </div>
+        <div class="col-3"></div>
+    </div>
+    `;
+    $('#result').html(result);
+}
 
-$('#donw').on('click', function(){
-    var getInput = $('#getInput').val();
-    userDonw(getInput); 
-});
-function userUp(up){
-    var getVarlueUp = parseInt(up) + 1;
-    if(getVarlueUp <= 15){
-        $('#getInput').val(getVarlueUp);
-        multiple(getVarlueUp);
-       
-    }
+function eachIngredients(ingredients){
+    $('#ingredients').html("Ingredients");
+    var result ="";
+    ingredients.forEach(element =>{
+        var {iconUrl, quantity, unit, name} = element;
+        result +=`
+    <tr>
+        <td><img src="${iconUrl}" width="30"></td>
+        <td>${quantity}</td>
+        <td>${(unit[0]).toLowerCase()}</td>
+        <td>${name}</td>
+    </tr>
+    `;
+    })
+    $('#output').html(result);
 }
-function userDonw(donw){
-    var getVarlueDonw = parseInt(donw) - 1;
-    if(getVarlueDonw >= 0){
-        $('#getInput').val(getVarlueDonw);
-        multiple(getVarlueDonw);
-        
-    }
-}
-function multiple(calculate){
-    var muls = calculate * 2;
-    output(muls);
-}
-function output(out){
-    $('#result').html(out);
+
+function eachStep(instructions){
+    $('#instructions').html("Instructions");
+    var result = "";
+    result +=`
+        <tr>
+            <td>${instructions}</td>
+        </tr>
+    `;
+    $('#instruct').html(result);
 }
